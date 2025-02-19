4295a4825ce1c1b80a1a1f22e72e02bd231db1fe
porcramines <porfiriones@gmail.com>
2025-02-18 19:16:19 -0500

    sesion 7 1

---
+++ S7/S7_1.php
@@ -0,0 +1,28 @@
+<?php
+//realizar una divison de un grupo de numeros
+//almacenamos los valores en cada variable
+
+$a=20;
+$b=18;
+$c=16;
+$d=14;
+$e=12;
+$f=10;
+
+//luego hacemos la division entre 2
+
+$a1=20/2;
+$b1=18/2;
+$c1=16/2;
+$d1=14/2;
+$e1=12/2;
+$f1=10/2;
+
+
+print " El siguiente ejercicio consiste en dividir entre 2 el grupo de datos, los cuales son:"."<br>";
+print $a.", ".$b.",".$c.",".$d.",".$e.",".$f;
+print "<br>";
+print "luego de hacer la division, el resultado es:"."<br>";
+print $a1."-".$b1."-".$c1."-".$d1."-".$e1."-".$f1;
+
+?>
